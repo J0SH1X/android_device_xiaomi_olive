9c23bf2203066b1454de68dfeccf76e9cb7df744
J0SH1X <aljoshua.hell@gmail.com>
2020-01-18 20:04:12 +0100

    Olive: add Xioami Doze

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Ambient display
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.a2dp.default
