3bbd5943d0cb9f7759285f8f9b53ab612504cb9d
J0SH1X <aljoshua.hell@gmail.com>
2020-01-18 19:51:32 +0100

    Olive: fix up lineage_olive.mk

--- lineage_olive.mk
+++ lineage_olive.mk
@@ -15,6 +15,7 @@
 #
 
 # Inherit from the common Open Source configuration.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
@@ -27,12 +28,12 @@ $(call inherit-product, device/xiaomi/olive/device.mk)
 PRODUCT_DEVICE := olive
 PRODUCT_NAME := lineage_olive
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi 7A
+PRODUCT_MODEL := Redmi 8
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="olive-user 9 PKQ1.190319.001 V10.2.5.0.PCMMIXM release-keys" \
+    PRIVATE_BUILD_DESC="olive-user 9 PKQ1.190319.001 V11.0.1.0.PCNEUXM release-keys" \
     PRODUCT_NAME="olive" \
     TARGET_DEVICE="olive"
 
-BUILD_FINGERPRINT := Xiaomi/olive/olive:9/PKQ1.190319.001/V10.2.5.0.PCMMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/olive_eea/olive:9/PKQ1.190319.001/V11.0.1.0.PCNEUXM:user/release-keys
